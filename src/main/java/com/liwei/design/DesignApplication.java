032a87f09e378c15f0d962603676bb7d6f2c49c7
liwei <liwei@youbanban.com>
2018-03-22 09:35:56 +0800

    update

--- src/main/java/com/liwei/design/DesignApplication.java
+++ src/main/java/com/liwei/design/DesignApplication.java
@@ -2,6 +2,10 @@ package com.liwei.design;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class DesignApplication {
@@ -9,4 +13,13 @@ public class DesignApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DesignApplication.class, args);
 	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize("35MB");
+		factory.setMaxRequestSize("35MB");
+		return factory.createMultipartConfig();
+	}
+
 }
