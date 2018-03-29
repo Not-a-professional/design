cc9d15835d5cee952d2e20ee600e7b3486e018f0
liwei <liwei@youbanban.com>
2018-03-29 11:49:00 +0800

    update 增加邮箱不可重复注册验证

--- src/main/java/com/liwei/design/repo/UserRepository.java
+++ src/main/java/com/liwei/design/repo/UserRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     @Query("select u from User u where u.username = ?1")
     User findAllByUsername(String username);
 
     @Query("select u from User u where u.email = ?1")
-    User findByEmail(String email);
+    List<User> findByEmail(String email);
 }
