5c71bb5667bbb476aa9a1c5dc063cca300f1b1f5
liwei <liwei@youbanban.com>
2018-05-03 16:34:56 +0800

    update 文本变为图片显示 js部分完成 未测试

--- src/main/resources/static/js/user/pic_vid.js
+++ src/main/resources/static/js/user/pic_vid.js
@@ -9,6 +9,24 @@ $(function init() {
         node = document.createElement("video");
         node.setAttribute("src", "http://localhost:8080/file/download?other&path=" + pvPath);
         node.setAttribute("controls", "controls");
+    } else if (suffix == 'txt') {
+        var temp = $("#hiddenForTxt");
+        var h;
+        $.ajax({
+            url:"/file/getEditorHtml?path=" + pvPath,
+            dataType:"json",
+            success: function (data) {
+                h = data['res'];
+            }
+        });
+        temp.append(h);
+        html2canvas([temp.get(0)], {
+            onrendered: function (canvas) {
+                var pngUrl = canvas.toDataURL("image/png");
+                node = document.createElement("img");
+                node.setAttribute("scr", pngUrl);
+            }
+        })
     }
     document.getElementById("pv").appendChild(node);
 });
